a5866bbb73d18f1fbd39e5d683dcecf4a97d25e0
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2026-02-16 15:50:32 +0100

    add run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+xhost +local:docker
+docker run --rm \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -e XAUTHORITY=$XAUTHORITY \
+    -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e 'QT_X11_NO_MITSHM=1' \
+    -e ROS_DOMAIN_ID=1 \
+    -e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v "$(dirname "$(readlink -f "$0")")/apriltag_pose":/ros2_ws/src/apriltag_pose:ro \
+    -v /dev/shm:/dev/shm \
+    --ipc host \
+    --device /dev/dri \
+    --net host \
+    --name apriltag_pose \
+    -ti inria_docker:orbbec \
+    bash 
