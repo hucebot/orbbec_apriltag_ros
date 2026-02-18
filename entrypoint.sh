1ea697e456ef0f13704e6343c70ecbe23c274acf
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2026-02-18 13:30:13 +0100

    add_docker_compose

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+echo "=== apriltag_pose entrypoint ==="
+
+source /opt/ros/humble/setup.bash
+
+unset RMW_IMPLEMENTATION
+
+cd /ros2_ws
+
+echo "=== Building apriltag_pose ==="
+colcon build --packages-select apriltag_pose
+source /ros2_ws/install/setup.bash
+
+echo "=== Launching apriltag_pose ==="
+exec ros2 launch apriltag_pose apriltag_pose.launch.py \
+    publishing_frame:=pelvis \
+    verbose:=true
